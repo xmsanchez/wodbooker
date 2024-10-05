31d203b9259b5f23e43c618e231be6d4798b93c5
Xavier Miranda Sánchez <xms.pixel@gmail.com>
2024-10-05 09:31:03 +0200

    fix: BookingPenalization exception not imported

--- wodbooker/scraper.py
+++ wodbooker/scraper.py
@@ -9,7 +9,7 @@ import pytz
 from bs4 import BeautifulSoup
 from .exceptions import LoginError, InvalidWodBusterResponse, \
     BookingNotAvailable, ClassIsFull, PasswordRequired, InvalidBox, \
-    ClassNotFound, BookingFailed
+    ClassNotFound, BookingFailed, BookingPenalization
 
 _HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
